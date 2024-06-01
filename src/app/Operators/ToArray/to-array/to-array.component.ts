1ec2d7b8fca502ca78af8f8f6801caf01ce3aa79
Mohsin Zaheer <msnoay@hotmail.com>
2024-06-01 08:29:45 +0500

    add

--- src/app/Operators/ToArray/to-array/to-array.component.ts
+++ src/app/Operators/ToArray/to-array/to-array.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { from, interval, take, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-to-array',
   templateUrl: './to-array.component.html',
-  styleUrl: './to-array.component.scss'
+  styleUrl: './to-array.component.scss',
 })
-export class ToArrayComponent {
+export class ToArrayComponent implements AfterViewInit {
+  obsArray: any = [];
+  userArray: any = [];
 
+  users = [
+    {
+      id: 1,
+      name: 'Ali Khan',
+      country: 'Pakistan',
+      email: 'ali.khan@example.com',
+    },
+    {
+      id: 2,
+      name: 'Hassan Ahmed',
+      country: 'Pakistan',
+      email: 'hassan.ahmed@example.com',
+    },
+    {
+      id: 3,
+      name: 'Shoaib Akhtar',
+      country: 'Pakistan',
+      email: 'shoaib.akhtar@example.com',
+    },
+    {
+      id: 4,
+      name: 'Sunny Leone',
+      country: 'India',
+      email: 'sunny.leone@example.com',
+    },
+    {
+      id: 5,
+      name: 'Qasim Ali',
+      country: 'Pakistan',
+      email: 'qasim.ali@example.com',
+    },
+  ];
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const obs1 = interval(1000);
+
+    obs1
+      .pipe(
+        //take operator will take only five elements from incoming data from interval
+        take(5),
+        toArray()
+      )
+      .subscribe((data) => {
+        this.obsArray = data;
+
+        console.log(this.obsArray);
+      });
+
+   
+  }
+
+  showUserData(){
+
+     //another example with array of objects
+     const obs3 = from(this.users);
+     obs3.pipe(toArray()).subscribe((user) => {
+       this.userArray = user;
+       console.log(this.userArray);
+     });
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { FromEventComponent } from './Operators/fromEvent/from-event/from-event.component';
 import { IntervalTimerComponent } from './Operators/IntervalTimer/interval-timer/interval-timer.component';
 import { OfAndFromComponent } from './Operators/OFandFrom/of-and-from/of-and-from.component';
+import { ToArrayComponent } from './Operators/ToArray/to-array/to-array.component';
 
 
 const routes: Routes = [
  {path:"",component:HomeComponent},
  {path:"fromEvent",component:FromEventComponent},
  {path:"interval",component:IntervalTimerComponent},
- {path:"ofAndFrom",component:OfAndFromComponent}
+ {path:"ofAndFrom",component:OfAndFromComponent},
+ {path:"toArray",component:ToArrayComponent}
 
 ];
 
